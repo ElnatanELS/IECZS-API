0848644ccd5b07e73a7b7dbbff81317986c823d1
ElnatanEls <elnatangalaxy@gmail.com>
2018-03-05 15:04:34 -0300

    Estrutura do Projeto e criação do Domain

---
+++ IECZS-API/src/main/java/br/com/congregacionalzonasul/IECZSAPI/Domain/Membro.java
@@ -0,0 +1,66 @@
+package br.com.congregacionalzonasul.IECZSAPI.Domain;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.OneToMany;
+
+import org.springframework.data.annotation.Id;
+
+public class Membro {
+	
+	@Id
+	private String id;
+	
+	private String nome;
+	
+	private Long rg;
+	
+	private Date dataDeNascimento;
+	
+	private List<String> departamentos;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getRg() {
+		return rg;
+	}
+
+	public void setRg(Long rg) {
+		this.rg = rg;
+	}
+
+	public Date getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+
+	public void setDataDeNascimento(Date dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+
+	@OneToMany
+	public List<String> getDepartamentos() {
+		return departamentos;
+	}
+
+	public void setDepartamentos(List<String> departamentos) {
+		this.departamentos = departamentos;
+	}
+	
+	
+
+}
---
+++ IECZS-API/src/main/java/br/com/congregacionalzonasul/IECZSAPI/IeczsApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.congregacionalzonasul.IECZSAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IeczsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IeczsApiApplication.class, args);
+	}
+}
